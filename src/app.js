a42a6dca30842a238bcf1ffd5b83d87f3c69ab0c
Melaku Belew <embd2026gc@gmail.com>
2026-05-01 17:43:03 +0300

    setup express server and environmental configuration

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import cookieParser from 'cookie-parser'
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan('dev'));
+app.use(helmet());
+
+
+app.get('/', (req, res)=>{
+    res.json('API Running......');
+})
+
+export default app;
