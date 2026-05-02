6da9f16a8e62544ea84560ec4588c766e5b22066
Melaku Belew <embd2026gc@gmail.com>
2026-05-02 12:16:55 +0300

    integrate routes module into app configuration

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import morgan from 'morgan'
 import helmet from 'helmet'
 import cookieParser from 'cookie-parser'
+import router from './routes/authRoute.js';
 
 const app = express();
 app.use(express.json());
@@ -9,6 +10,7 @@ app.use(cookieParser());
 app.use(morgan('dev'));
 app.use(helmet());
 
+app.use('/api/auth', router);
 
 app.get('/', (req, res)=>{
     res.json('API Running......');
