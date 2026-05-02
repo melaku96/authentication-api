1f476a3a8e95e84bda1b1edce3037ff1e077d75d
Melaku Belew <embd2026gc@gmail.com>
2026-05-02 16:07:05 +0300

    integrate reset password end points to routes

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { registerUser, loginUsr } from '../controllers/authController.js';
 import { refreshToken } from '../utils/refreshToken.js';
+import { forgotPassword, resetPassword } from '../utils/resetPassword.js';
 
 const router = express.Router();
 
@@ -8,5 +9,7 @@ const router = express.Router();
 router.post('/register', registerUser);
 router.post('/login', loginUsr);
 router.post('/refresh', refreshToken)
+router.post('/forgot-password', forgotPassword)
+router.put('/reset-password/:token', resetPassword)
 
 export default router;
