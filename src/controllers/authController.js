07376b26e0608a2e5c1e9d26138fce0e920cf5a3
Melaku Belew <embd2026gc@gmail.com>
2026-05-02 11:43:55 +0300

    implement user login with token generation

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,10 @@
 import bcrypt from "bcryptjs";
 import {registerJoi} from '../utils/validation.js'
-import userModel from "../../../../../###PROJECTS/NodeJs Files/Product_API/models/userModel.js";
+import userModel from "../model/userModel.js";
+import cookieParser from "cookie-parser";
+import { generateAccessToken, generateRefreshToken } from "../utils/generateToken.js";
 
+//SIGN UP
 export const registerUser = async(req, res)=>{
     try {
        const {error} = registerJoi.validate(req.body, {abortEarly: false});
@@ -30,6 +33,47 @@ export const registerUser = async(req, res)=>{
         message: 'User registered successfully',
         user
        })  
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+}
+
+// LOGIN
+export const loginUsr = async(req, res)=>{
+    try {
+        const {email, password} = req.body;
+        //check existance
+        const user = await userModel.findOne({email});
+        if(!user || !(await bcrypt.compare(password, user.password))){
+            return res.status(401).json({
+                success: false,
+                message: 'Invalid credential'
+            });
+        };
+        const accessToken = generateAccessToken(user);
+        const refreshToken = generateRefreshToken(user);
+
+        // hash refreshToken
+        const hashedRefreshToken = await bcrypt.hash(refreshToken, 10);
+
+        //update user
+        user.refreshToken = hashedRefreshToken;
+        await user.save();
+
+        res.cookie('refreshToken', hashedRefreshToken, {
+            httpOnly: true,
+            secure: false,
+            sameSite: 'strict'
+        });
+        res.status(200).json({
+            success: true,
+            message: 'Login Successfully!',
+            accessToken:accessToken
+        });
+
     } catch (error) {
         res.status(500).json({
             success: false,
