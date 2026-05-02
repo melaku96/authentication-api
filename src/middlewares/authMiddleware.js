c2f6313c84f601676fdfa3112b53261a32cb75fe
Melaku Belew <embd2026gc@gmail.com>
2026-05-02 16:23:31 +0300

    add authentication middleware

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken'
+
+export const authMiddleware = (req, res, next)=>{
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+    if(!token){
+        return res.status(400).json({
+            success: false,
+            message: 'No token found'
+        })
+    }
+    const decoded = jwt.verify(token, process.env.JWT_ACCESS_TOKEN_SECRET);
+    userDecoded = decoded;
+    next();
+
+    } catch (error) {
+           res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+
+}
